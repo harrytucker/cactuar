24d666d86efb8800284d95692455a895a9441b9a
Harry Tucker <harrytuckerubuntu@gmail.com>
2023-03-23 21:51:15 +0000

    Remove placeholder values from replica_alerts

--- src/prometheus/replica_alerts.rs
+++ src/prometheus/replica_alerts.rs
@@ -1,8 +1,6 @@
 use crate::crd::{AlertConfig, Operation, ServiceAlertSpec};
 
-use super::alert::{
-    AlertGroup, AlertRules, Annotations, Labels, PrometheusSeverity, PLACEHOLDER_VALUE,
-};
+use super::alert::{AlertGroup, AlertRules, Annotations, Labels, PrometheusSeverity};
 
 pub fn replica_count_rules(alert_configs: &[AlertConfig], spec: &ServiceAlertSpec) -> AlertGroup {
     // Prometheus Alert Rules in a single file must be uniquely named, but we
@@ -12,7 +10,7 @@ pub fn replica_count_rules(alert_configs: &[AlertConfig], spec: &ServiceAlertSpe
         .iter()
         .enumerate()
         .map(|(i, conf)| AlertRules {
-            alert: format!("ReplicaCountRules-{0}-{1}", spec.deployment_name, i),
+            alert: format!("ReplicaRule-{0}-{1}", spec.deployment_name, i),
             expr: replicas_promql(conf, spec),
             for_: conf.for_.clone(),
             labels: Labels {
@@ -20,15 +18,12 @@ pub fn replica_count_rules(alert_configs: &[AlertConfig], spec: &ServiceAlertSpe
                 source: spec.common_labels.origin.clone(),
                 owner: spec.common_labels.owner.clone(),
             },
-            annotations: Annotations {
-                summary: PLACEHOLDER_VALUE.into(),
-                description: PLACEHOLDER_VALUE.into(),
-            },
+            annotations: replicas_annotations(conf),
         })
         .collect();
 
     AlertGroup {
-        name: PLACEHOLDER_VALUE.into(),
+        name: String::from("Replica Alerts"),
         rules: replica_rules,
     }
 }
@@ -61,5 +56,30 @@ fn replicas_promql(alert_config: &AlertConfig, spec: &ServiceAlertSpec) -> Strin
             r#"sum by (app_kubernetes_io_name) (up{{app_kubernetes_io_name="{0}"}}) > {1}"#,
             spec.deployment_name, alert_config.value,
         ),
+    }
+}
+
+fn replicas_annotations(alert_config: &AlertConfig) -> Annotations {
+    match alert_config.operation {
+        Operation::EqualTo => Annotations {
+            summary: String::from("Replicas reached alert boundary"),
+            description: format!("{0} replicas currently up", alert_config.value),
+        },
+        Operation::LessThan => Annotations {
+            summary: String::from("Replicas less than alert boundary"),
+            description: format!(
+                // quad-braces necessary for escaping within format! macro
+                "{{{{ $value }}}} replicas currently up, expected at least {0}",
+                alert_config.value
+            ),
+        },
+        Operation::MoreThan => Annotations {
+            summary: String::from("Replicas more than alert boundary"),
+            description: format!(
+                // quad-braces necessary for escaping within format! macro
+                "{{{{ $value }}}} replicas currently up, expected less than {0}",
+                alert_config.value
+            ),
+        },
     }
 }
