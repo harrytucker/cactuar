e080fd4920dafc7c56cacd6e95708c5e3a3b3bf6
Adam Rummer <31801759+cyclingwithelephants@users.noreply.github.com>
2023-11-14 15:46:17 +0000

    cargo fmt

--- src/prometheus/alert.rs
+++ src/prometheus/alert.rs
@@ -5,7 +5,9 @@ use serde::{Deserialize, Serialize};
 
 use crate::{
     crd::{ReplicaAlert, ServiceAlertSpec},
-    prometheus::{http_alerts::http_rules, grpc_alerts::grpc_alert_rules, replica_alerts::replica_count_rules},
+    prometheus::{
+        grpc_alerts::grpc_alert_rules, http_alerts::http_rules, replica_alerts::replica_count_rules,
+    },
 };
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
